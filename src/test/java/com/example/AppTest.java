2e224e4895b8bd6cde4015f1231313907b1253e7
Vishva-2410 <patilvishu2410@gmail.com>
2026-08-19 20:23:18 +0530

    created files for project of java app

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package src.test.java.com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
